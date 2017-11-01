2286068baaaefe326bcbd60bd319ba025323453e
beth92 <bethtownsend92@gmail.com>
2017-11-01 16:05:55 -0400

    Started API usage

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 /*jshint esversion: 6 */
 
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs.options({
+  a: {
+    demand: true,
+    alias: 'address',
+    describe: 'Address to fetch weather for',
+    string: true
+  }
+})
+.help()
+.alias('help', 'h')
+.argv;
 
 request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyAoclgzqaxH1vXgsXYt-9Y-T7nCVuus38s',
   //specifies that data returned should be treated as an object
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
