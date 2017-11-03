6abc4e95cd665b048bd99dddff47b751047faa09
beth92 <bethtownsend92@gmail.com>
2017-11-03 11:47:25 -0400

    Error handling in request

--- app.js
+++ app.js
@@ -15,12 +15,23 @@ const argv = yargs.options({
 .alias('help', 'h')
 .argv;
 
+const api_key='AIzaSyAoclgzqaxH1vXgsXYt-9Y-T7nCVuus38s';
+var encodedAddress = encodeURIComponent(argv.address);
+
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyAoclgzqaxH1vXgsXYt-9Y-T7nCVuus38s',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${api_key}`,
   //specifies that data returned should be treated as an object
   json: true
 }, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  if(error){
+    console.log('Unable to connect to geolocation server.');
+  } else if(body.status==='ZERO_RESULTS'){
+    console.log('Unable to find that address.');
+  } else if(body.status=='OK'){
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+  } else {
+    console.log(`Unknown error: status returned by server: ${body.status}`);
+  }
 });
