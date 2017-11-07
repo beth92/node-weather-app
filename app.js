51e89c1be750e0c576c36f2c59b9bd88c44eb503
beth92 <bethtownsend92@gmail.com>
2017-11-07 11:15:02 -0500

    Abstracted geocode function to own module

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 const request = require('request');
 const yargs = require('yargs');
 
+
+const geocode = require('./geocode/geocode');
+
 const argv = yargs.options({
   a: {
     demand: true,
@@ -15,23 +18,10 @@ const argv = yargs.options({
 .alias('help', 'h')
 .argv;
 
-const api_key='AIzaSyAoclgzqaxH1vXgsXYt-9Y-T7nCVuus38s';
-var encodedAddress = encodeURIComponent(argv.address);
-
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${api_key}`,
-  //specifies that data returned should be treated as an object
-  json: true
-}, (error, response, body) => {
-  if(error){
-    console.log('Unable to connect to geolocation server.');
-  } else if(body.status==='ZERO_RESULTS'){
-    console.log('Unable to find that address.');
-  } else if(body.status=='OK'){
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+geocode.geocodeAddress(argv.address, (errorMessage, results)=>{
+  if (errorMessage){
+    console.log(errorMessage);
   } else {
-    console.log(`Unknown error: status returned by server: ${body.status}`);
+    console.log(JSON.stringify(results, undefined, 4));
   }
 });
